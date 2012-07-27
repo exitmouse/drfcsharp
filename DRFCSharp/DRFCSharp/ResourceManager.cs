55f9646d3d63f518a81be652a911a8b33e73e8b4
Jesse Selover <jesse.s.selover.15@dartmouth.edu>
2012-07-27 17:29:13 -0400

    Move ResourceManagerBuilder to ResourceManager class (use a nested class).

--- DRFCSharp/DRFCSharp/ResourceManager.cs
+++ DRFCSharp/DRFCSharp/ResourceManager.cs
@@ -8,6 +8,21 @@ namespace DRFCSharp
 		public string ImgPath { get; private set; }
 		public string LabelPath { get; private set; }
 		private static string feature_test_path = string.Format("{0}/../../../../TestImages/",AppDomain.CurrentDomain.BaseDirectory); //Added first forward slash to make it work on linux.
+
+        public class Builder
+        {		
+            public string ImgPath { get; set; }
+            public string LabelPath { get; set; }
+            public Builder()
+            {
+                ImgPath = string.Format("{0}../../../../Dataset/",AppDomain.CurrentDomain.BaseDirectory);
+                LabelPath = string.Format("{0}../../../../Dataset/",AppDomain.CurrentDomain.BaseDirectory);
+            }
+            public ResourceManager Build(){
+                return new ResourceManager(ImgPath, LabelPath);
+            }
+        }
+
 		public ResourceManager(string img_path, string label_path)
 		{
 			ImgPath = img_path;
