04d5b145829b0eeb885ac13809352d5c065213c6
Jesse Selover <jesse.s.selover.15@dartmouth.edu>
2013-02-11 23:05:50 -0800

    Elucidate meaning of sigma.

--- DRFCSharp/DRFCSharp/MathWrapper.cs
+++ DRFCSharp/DRFCSharp/MathWrapper.cs
@@ -1,31 +1,38 @@
 using System;
 namespace DRFCSharp
 {
-	/// <summary>
-	/// Wraps around a few basic math functions to shield from numerical errors.
-	/// </summary>
-	public sealed class MathWrapper
-	{
-		private MathWrapper()
-		{
-		}
-		public static double Exp(double x){ return Math.Exp(x); }
-		public static double Log(double x){ 
-			if(x <= double.Epsilon)
-			{
-				return Math.Log (double.Epsilon);
-			}
-			return Math.Log(x);
-		}
-		public static double Sigma(double x){
-			double result = 1/(1+Exp(-x));
-			if(result < 0 || result > 1)
-			{
-				throw new NotFiniteNumberException("Sigma should be between 0 and 1");
-			}
-			if(result < double.Epsilon) return double.Epsilon;
-			return result; 
-		}
-	}
+    /// <summary>
+    /// Wraps around a few basic math functions to shield from numerical errors.
+    /// </summary>
+    public sealed class MathWrapper
+    {
+        private MathWrapper()
+        {
+        }
+        public static double Exp(double x){ return Math.Exp(x); }
+        public static double Log(double x){ 
+            if(x <= double.Epsilon)
+            {
+                throw new ArgumentException("Log of number smaller than epsilon.");
+                return Math.Log (double.Epsilon);
+            }
+            return Math.Log(x);
+        }
+        // Logistic function
+        public static double Sigma(double x){
+            double result = 1/(1+Exp(-x));
+            if(result < 0 || result > 1)
+            {
+                throw new NotFiniteNumberException("Sigma should be between 0 and 1");
+            }
+            if(result < double.Epsilon) {
+                return double.Epsilon;
+            }
+            if(result > 1 - 2*double.Epsilon) {
+                throw new ArgumentException("Too close to 1");
+            } 
+            return result; 
+        }
+    }
 }
 
